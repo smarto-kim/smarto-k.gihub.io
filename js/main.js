2a4f63dee8c8632f50227055d9f0519b1329cc87
martin <martow17@gmail.com>
2016-04-04 06:17:41 -0800

    add loader

--- js/main.js
+++ js/main.js
@@ -561,3 +561,14 @@ $.Velocity
             [ { translateY: '-50%'}, 1]
         ]
     });
+    
+    
+   //main loader css
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+});
