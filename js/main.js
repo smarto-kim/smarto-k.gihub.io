d9046de9589a7610982c4f67955980f50ab293c5
martin <martow17@gmail.com>
2016-04-05 09:46:16 -0800

    add home page

--- js/main.js
+++ js/main.js
@@ -561,14 +561,3 @@ $.Velocity
             [ { translateY: '-50%'}, 1]
         ]
     });
-    
-    
-   //main loader css
-$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
-});
